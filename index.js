7645d00154cbefb080a207dbeb42cccdb58e38e3
daniela <dani.kuroda@gmail.com>
2023-10-22 14:11:22 -0300

    Entrega do desafio finalizado

--- index.js
+++ index.js
@@ -17,3 +17,28 @@ Se vitórias for maior ou igual a 101 = Imortal
 Ao final deve se exibir uma mensagem:
 "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
 */
+
+function rankeador(qtdeVitorias, qtdeDerrotas) {
+    return qtdeVitorias - qtdeDerrotas;
+  }
+  
+  let ranking = rankeador(300, 220); 
+  let nivel;
+
+  if (ranking <= 10) {
+    nivel = "Ferro"
+} else if (ranking > 11 && ranking <= 20) {
+    nivel = "Bronze"
+} else if (ranking > 21 && ranking <= 50) {
+    nivel = "Prata"
+} else if (ranking > 51 && ranking <= 80) {
+    nivel = "Ouro"
+} else if (ranking > 81 && ranking <= 90) {
+    nivel = "Platina"
+} else if (ranking > 91 && ranking <= 100) {
+    nivel = "Ascendente"
+} else if (ranking > 101) {
+    nivel = "Radiante"
+}
+
+console.log("Seu ranking é: " + nivel + ", com " + ranking + " pontos.");
